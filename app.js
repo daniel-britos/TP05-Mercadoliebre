30707b081b03266a609ef915103b91c26f16c5e7
daniel-britos <danielbritospersonal@gmail.com>
2022-04-19 20:18:30 -0300

    agrega formulario

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     return res.sendFile(path.resolve(__dirname, 'views', 'home.html'));
 });
-
+app.get('/register', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, 'views', 'register.html'));
+});
+app.get('/login', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, 'views', 'login.html'));
+});
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${port}`);
